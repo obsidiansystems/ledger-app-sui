55935c28c965126049ea173850f528d1a8a0f132
John Ericson <John.Ericson@Obsidian.Systems>
2022-01-12 08:56:08 +0000

    Try out new support to -Z build-std=core from Nix

--- default.nix
+++ default.nix
@@ -9,22 +9,31 @@ rec {
 
   app = import ./Cargo.nix {
     pkgs = ledgerPkgs;
-    buildRustCrateForPkgs = pkgs: (buildRustCrateForPkgsLedger pkgs).override {
-      defaultCrateOverrides = pkgs.defaultCrateOverrides // {
-        nanos_sdk = _: {
-          RUSTC_BOOTSTRAP = true;
-        };
-        rust-app = attrs: let
-          sdk = lib.findFirst (p: lib.hasPrefix "rust_nanos_sdk" p.name) (builtins.throw "no sdk!") attrs.dependencies;
-        in {
-          preHook = ledger-platform.gccLibsPreHook;
-          extraRustcOpts = [
-            "-C" "relocation-model=ropi"
-            "-C" "link-arg=-T${sdk.lib}/lib/nanos_sdk.out/script.ld"
-          ];
+    buildRustCrateForPkgs = pkgs: let
+      fun = (buildRustCrateForPkgsLedger pkgs).override {
+        defaultCrateOverrides = pkgs.defaultCrateOverrides // {
+          nanos_sdk = _: {
+            RUSTC_BOOTSTRAP = true;
+          };
+          rust-app = attrs: let
+            sdk = lib.findFirst (p: lib.hasPrefix "rust_nanos_sdk" p.name) (builtins.throw "no sdk!") attrs.dependencies;
+          in {
+            preHook = ledger-platform.gccLibsPreHook;
+            extraRustcOpts = [
+              "-C" "relocation-model=ropi"
+              "-C" "link-arg=-T${sdk.lib}/lib/nanos_sdk.out/script.ld"
+              "-C" "linker=${pkgs.stdenv.cc.targetPrefix}lld"
+            ];
+          };
         };
       };
-    };
+    in
+      args: fun (args // lib.optionalAttrs pkgs.stdenv.hostPlatform.isAarch32 {
+        dependencies = map (d: d // { stdlib = true; }) [
+          ledger-platform.ledgerCore
+          ledger-platform.ledgerCompilerBuiltins
+        ] ++ args.dependencies;
+      });
   };
 
   # For CI
