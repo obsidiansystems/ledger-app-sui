bfcf2badcb79343fedeb2c952c4657686f4f190f
John Ericson <John.Ericson@Obsidian.Systems>
2022-01-13 22:16:57 +0000

    Bump ledger platform, build app.hex

--- default.nix
+++ default.nix
@@ -36,4 +36,28 @@ rec {
 
   # For CI
   rootCrate = app.rootCrate.build;
+
+  tarSrc = ledgerPkgs.runCommandCC "tarSrc" {
+    nativeBuildInputs = [
+      ledger-platform.cargo-ledger
+      ledger-platform.ledgerRustPlatform.rust.cargo
+    ];
+  } (ledger-platform.cargoLedgerPreHook + ''
+
+    cp ${./rust-app/Cargo.toml} ./Cargo.toml
+    # So cargo knows it's a binary
+    mkdir src
+    touch src/main.rs
+
+    cargo-ledger --use-prebuilt ${rootCrate}/bin/rust-app --hex-next-to-json
+
+    mkdir -p $out/rust-app
+    cp app.json app.hex $out/rust-app
+    cp ${./tarball-default.nix} $out/rust-app/default.nix
+    cp ${./rust-app/crab.gif} $out/rust-app/crab.gif
+  '');
+
+  tarball = pkgs.runCommandNoCC "app-tarball.tar.gz" { } ''
+    tar -czvhf $out -C ${tarSrc} rust-app
+  '';
 }
