34c06da8f7d10a270df2e6fd8db1331411761210
Divam <dfordivam@gmail.com>
2022-08-17 18:35:21 +0900

    ts-tests: run app_main directly, as the EXE somehow does not run app_main

--- rust-app/tests/ts-tests.rs
+++ rust-app/tests/ts-tests.rs
@@ -3,14 +3,13 @@
 #![test_runner(crate::my_runner)]
 #![no_main]
 
-use nanos_sdk::{debug_print, exit_app};
+use nanos_sdk::{exit_app};
+
+use rust_app::main_nanos::*;
 
 #[no_mangle]
 extern "C" fn sample_main() {
-    debug_print("\nEXE: ");
-    debug_print(EXE_PATH);
-    debug_print("\n");
-    exit_app(0);
+    app_main()
 }
 
 fn my_runner(_: &[&i32]) {
@@ -21,9 +20,3 @@ use core::panic::PanicInfo;
 fn panic_handler(_: &PanicInfo) -> ! {
     exit_app(0);
 }
-
-pub static EXE_PATH : &str = env!("CARGO_BIN_EXE_rust-app");
-
-
-// Stub to trigger a build with ts-tests; this will actually be intercepted by speculos-wrapper to
-// as a trigger to run the typescript tests.
