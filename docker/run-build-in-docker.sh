5d3eaacf37150ee9e18499e527ed0d17eda91164
Divam <dfordivam@gmail.com>
2022-12-08 14:37:13 +0900

    Scripts to build via docker nightly toolchain and test via nix

---
+++ docker/run-build-in-docker.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -eu
+
+OUT_DIR=/app/docker-outputs
+
+RUST_NANOS_SDK=`mktemp -d`
+TARGET_DIR=`mktemp -d`
+
+git clone "$RUST_NANOS_SDK_GIT" "$RUST_NANOS_SDK"
+cd "$RUST_NANOS_SDK"; git checkout "$RUST_NANOS_SDK_REV"; cd -
+
+PATH=$RUST_NANOS_SDK:$PATH
+export OBJCOPY="llvm-objcopy"
+export NM="llvm-nm"
+
+cd rust-app
+
+for device in nanos nanosplus nanox
+do
+   cargo +nightly build --target-dir=$TARGET_DIR --release --target=$device.json -Z build-std=core
+   cp $TARGET_DIR/$device/release/$APP_NAME $OUT_DIR/$device
+   chown $HOST_UID:$HOST_GID $OUT_DIR/$device/$APP_NAME
+done
