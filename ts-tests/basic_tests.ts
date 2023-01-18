dccc0f0bac1dfb2d1bddf9f75b43f7cc36643bfa
Divam <dfordivam@gmail.com>
2023-01-18 17:04:10 +0900

    Add a version test, for coverage

--- ts-tests/basic_tests.ts
+++ ts-tests/basic_tests.ts
@@ -84,3 +84,14 @@ describe("Signing tests", function() {
        ]
      ));
 });
+
+describe("get version tests", function() {
+  it("can get app version", async () => {
+    await sendCommandAndAccept(async (client : any) => {
+      var rv = await client.getVersion();
+      expect(rv.major).to.equal(0);
+      expect(rv.minor).to.equal(0);
+      expect(rv.patch).to.equal(1);
+      }, []);
+    });
+});
