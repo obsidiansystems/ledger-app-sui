e983def5a21d02a6325b34862bd4a82322bea84e
John Ericson <John.Ericson@Obsidian.Systems>
2023-01-31 19:31:17 +0000

    Nano S with logging passes CI now!

--- release.nix
+++ release.nix
@@ -4,6 +4,6 @@ in
   {
     generic-cli = self.alamgu.generic-cli;
   }
-  // lib.mapAttrs' (n: lib.nameValuePair ("nanos--" + n)) (builtins.removeAttrs self.nanos ["test-with-logging" "stack-check-with-logging"])
+  // lib.mapAttrs' (n: lib.nameValuePair ("nanos--" + n)) self.nanos
   // lib.mapAttrs' (n: lib.nameValuePair ("nanox--" + n)) self.nanox
   // lib.mapAttrs' (n: lib.nameValuePair ("nanosplus--" + n)) self.nanosplus
