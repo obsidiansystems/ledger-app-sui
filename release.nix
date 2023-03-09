6c3d7c01bc90a5d75e852e2755e63807c05c25e8
John Ericson <John.Ericson@Obsidian.Systems>
2023-03-09 16:08:51 +0000

    Take Dylan's suggestion to make `release.nix` shorter

--- release.nix
+++ release.nix
@@ -3,21 +3,18 @@ let
   pkgsSrc = import (alamgu-path + "/dep/nixpkgs/thunk.nix");
   lib = import (pkgsSrc + "/lib");
 
-  x86_64-linux = import ./. rec {
-    localSystem = { system = "x86_64-linux"; };
-  };
-  x86_64-darwin = import ./. rec {
-    localSystem = { system = "x86_64-darwin"; };
-  };
+  perSystem = lib.genAttrs [ "x86_64-linux" "x86_64-darwin" ] (system: import ./. {
+     localSystem = { inherit system; };
+  });
 in {
-  inherit x86_64-linux x86_64-darwin;
+  inherit (perSystem) x86_64-linux x86_64-darwin;
 }
   # Hack until CI will traverse contents
-  // lib.mapAttrs' (n: lib.nameValuePair ("linux--" + n)) x86_64-linux
-  // lib.mapAttrs' (n: lib.nameValuePair ("linux--nanos--" + n)) x86_64-linux.nanos
-  // lib.mapAttrs' (n: lib.nameValuePair ("linux--nanox--" + n)) x86_64-linux.nanox
-  // lib.mapAttrs' (n: lib.nameValuePair ("linux--nanosplus--" + n)) x86_64-linux.nanosplus
-  // lib.mapAttrs' (n: lib.nameValuePair ("macos--" + n)) x86_64-darwin
-  // lib.mapAttrs' (n: lib.nameValuePair ("macos--nanos--" + n)) x86_64-darwin.nanos
-  // lib.mapAttrs' (n: lib.nameValuePair ("macos--nanox--" + n)) x86_64-darwin.nanox
-  // lib.mapAttrs' (n: lib.nameValuePair ("macos--nanosplus--" + n)) x86_64-darwin.nanosplus
+  // lib.mapAttrs' (n: lib.nameValuePair ("linux--" + n)) perSystem.x86_64-linux
+  // lib.mapAttrs' (n: lib.nameValuePair ("linux--nanos--" + n)) perSystem.x86_64-linux.nanos
+  // lib.mapAttrs' (n: lib.nameValuePair ("linux--nanox--" + n)) perSystem.x86_64-linux.nanox
+  // lib.mapAttrs' (n: lib.nameValuePair ("linux--nanosplus--" + n)) perSystem.x86_64-linux.nanosplus
+  // lib.mapAttrs' (n: lib.nameValuePair ("macos--" + n)) perSystem.x86_64-darwin
+  // lib.mapAttrs' (n: lib.nameValuePair ("macos--nanos--" + n)) perSystem.x86_64-darwin.nanos
+  // lib.mapAttrs' (n: lib.nameValuePair ("macos--nanox--" + n)) perSystem.x86_64-darwin.nanox
+  // lib.mapAttrs' (n: lib.nameValuePair ("macos--nanosplus--" + n)) perSystem.x86_64-darwin.nanosplus
