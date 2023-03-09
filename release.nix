b169841a43cc3da76cd0829f85ef89b75d050c7e
John Ericson <John.Ericson@Obsidian.Systems>
2023-03-09 07:25:25 +0000

    WIP: Fix macOS build

--- release.nix
+++ release.nix
@@ -1,9 +1,23 @@
-let self = import ./. { localSystem.system = "x86_64-linux"; };
-    lib = self.pkgs.lib;
-in
-  {
-    generic-cli = self.alamgu.generic-cli;
-  }
-  // lib.mapAttrs' (n: lib.nameValuePair ("nanos--" + n)) self.nanos
-  // lib.mapAttrs' (n: lib.nameValuePair ("nanox--" + n)) self.nanox
-  // lib.mapAttrs' (n: lib.nameValuePair ("nanosplus--" + n)) self.nanosplus
+let
+  alamgu-path = import ./dep/alamgu/thunk.nix;
+  pkgsSrc = import (alamgu-path + "/dep/nixpkgs/thunk.nix");
+  lib = import (pkgsSrc + "/lib");
+
+  x86_64-linux = import ./. rec {
+    localSystem = { system = "x86_64-linux"; };
+  };
+  x86_64-darwin = import ./. rec {
+    localSystem = { system = "x86_64-darwin"; };
+  };
+in {
+  inherit x86_64-linux x86_64-darwin;
+}
+  # Hack until CI will traverse contents
+  // lib.mapAttrs' (n: lib.nameValuePair ("linux--" + n)) x86_64-linux
+  // lib.mapAttrs' (n: lib.nameValuePair ("linux--nanos--" + n)) x86_64-linux.nanos
+  // lib.mapAttrs' (n: lib.nameValuePair ("linux--nanox--" + n)) x86_64-linux.nanox
+  // lib.mapAttrs' (n: lib.nameValuePair ("linux--nanosplus--" + n)) x86_64-linux.nanosplus
+  // lib.mapAttrs' (n: lib.nameValuePair ("macos--" + n)) x86_64-darwin
+  // lib.mapAttrs' (n: lib.nameValuePair ("macos--nanos--" + n)) x86_64-darwin.nanos
+  // lib.mapAttrs' (n: lib.nameValuePair ("macos--nanox--" + n)) x86_64-darwin.nanox
+  // lib.mapAttrs' (n: lib.nameValuePair ("macos--nanosplus--" + n)) x86_64-darwin.nanosplus
