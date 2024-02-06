43a0a5c95ab93d480da05b97c01d40222f42f7ec
Divam <dfordivam@gmail.com>
2024-02-06 13:07:36 +0900

    Use ledger_device_sdk 1.5.0 having relocations support

--- docker/run-build-in-docker.sh
+++ docker/run-build-in-docker.sh
@@ -9,10 +9,7 @@ TARGET_DIR=`mktemp -d`
 git clone "$RUST_NANOS_SDK_GIT" "$RUST_NANOS_SDK"
 cd "$RUST_NANOS_SDK"; git checkout "$RUST_NANOS_SDK_REV"; cd -
 
-# stat present in docker does not recognize --format
-sed -i 's/stat --format/stat -c/' $RUST_NANOS_SDK/ledger_device_sdk/scripts/link_wrap.sh
-
-PATH=$RUST_NANOS_SDK/ledger_device_sdk/scripts:$PATH
+PATH=$RUST_NANOS_SDK/ledger_device_sdk:$PATH
 export OBJCOPY="llvm-objcopy"
 export NM="llvm-nm"
 
