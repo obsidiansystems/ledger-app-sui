b3bd63aeab3de0d510f4a41c273a798a17ac3be4
Divam <dfordivam@gmail.com>
2024-02-07 16:37:38 +0900

    Disable stack-check on macos

--- release.nix
+++ release.nix
@@ -15,6 +15,6 @@ in {
   // lib.mapAttrs' (n: lib.nameValuePair ("linux--nanox--" + n)) (builtins.removeAttrs perSystem.x86_64-linux.nanox [])
   // lib.mapAttrs' (n: lib.nameValuePair ("linux--nanosplus--" + n)) (builtins.removeAttrs perSystem.x86_64-linux.nanosplus [])
   // lib.mapAttrs' (n: lib.nameValuePair ("macos--" + n)) perSystem.x86_64-darwin
-  // lib.mapAttrs' (n: lib.nameValuePair ("macos--nanos--" + n)) (builtins.removeAttrs perSystem.x86_64-darwin.nanos ["test" "test-with-logging" "rustShell"])
-  // lib.mapAttrs' (n: lib.nameValuePair ("macos--nanox--" + n)) (builtins.removeAttrs perSystem.x86_64-darwin.nanox ["test" "test-with-logging" "rustShell"])
-  // lib.mapAttrs' (n: lib.nameValuePair ("macos--nanosplus--" + n)) (builtins.removeAttrs perSystem.x86_64-darwin.nanosplus ["test" "test-with-logging" "rustShell"])
+  // lib.mapAttrs' (n: lib.nameValuePair ("macos--nanos--" + n)) (builtins.removeAttrs perSystem.x86_64-darwin.nanos ["stack-check" "stack-check-with-logging" "test" "test-with-logging" "rustShell"])
+  // lib.mapAttrs' (n: lib.nameValuePair ("macos--nanox--" + n)) (builtins.removeAttrs perSystem.x86_64-darwin.nanox ["stack-check" "stack-check-with-logging" "test" "test-with-logging" "rustShell"])
+  // lib.mapAttrs' (n: lib.nameValuePair ("macos--nanosplus--" + n)) (builtins.removeAttrs perSystem.x86_64-darwin.nanosplus ["stack-check" "stack-check-with-logging" "test" "test-with-logging" "rustShell"])
