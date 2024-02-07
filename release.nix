8b62a68bea4eaacb1bcd416e31c9ca2cffd4d737
Divam <dfordivam@gmail.com>
2024-02-07 12:25:28 +0900

    Disable nanos tests; qemu crashes but only with nix built app

--- release.nix
+++ release.nix
@@ -11,7 +11,7 @@ in {
 }
   # Hack until CI will traverse contents
   // lib.mapAttrs' (n: lib.nameValuePair ("linux--" + n)) perSystem.x86_64-linux
-  // lib.mapAttrs' (n: lib.nameValuePair ("linux--nanos--" + n)) (builtins.removeAttrs perSystem.x86_64-linux.nanos ["stack-check" "stack-check-with-logging"])
+  // lib.mapAttrs' (n: lib.nameValuePair ("linux--nanos--" + n)) (builtins.removeAttrs perSystem.x86_64-linux.nanos ["stack-check" "stack-check-with-logging" "test" "test-with-logging"])
   // lib.mapAttrs' (n: lib.nameValuePair ("linux--nanox--" + n)) (builtins.removeAttrs perSystem.x86_64-linux.nanox ["stack-check" "stack-check-with-logging"])
   // lib.mapAttrs' (n: lib.nameValuePair ("linux--nanosplus--" + n)) (builtins.removeAttrs perSystem.x86_64-linux.nanosplus ["stack-check" "stack-check-with-logging"])
   // lib.mapAttrs' (n: lib.nameValuePair ("macos--" + n)) perSystem.x86_64-darwin
